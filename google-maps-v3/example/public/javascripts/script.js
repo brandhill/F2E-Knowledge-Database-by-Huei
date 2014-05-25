cab76834e3495f760d5da10ead3fb1c58cd51e98
Huei Tan <huei90@gmail.com>
2014-05-25 20:00:59 +0800

    Update maps code

--- google-maps-v3/example/public/javascripts/script.js
+++ google-maps-v3/example/public/javascripts/script.js
@@ -1,50 +1,134 @@
 (function () {
 
-    var map,
-      markers = [],
-      circles = [];
+    angular.module('myApp', [])
+      .provider('utility', function () {
 
-    function initialMap() {
-        var pos = new google.maps.LatLng(25.0392179,121.5398562);
+          this.spinner = {
+              on: function () {
+                  $('.loading').show();
+              },
+              off: function () {
+                  $('.loading').hide();
+              }
+          };
 
-        google.maps.visualRefresh = true;
+          this.$get = [function () {
+            return {
+                spinner: this.spinner
+            }
+          }];
+      })
+      .controller('indexController', ['utility', function (utility) {
+          var map,
+              infowindow,
+              markers = [],
+              umarker = [],
+              circles = [];
 
-        var mapOptions = {
-            zoom: 15,
-            disableDefaultUI: true,
-            center: pos,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+          function createMarker(place) {
 
-        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: place.pos,
+                  animation: google.maps.Animation.DROP
+              });
 
-        var input = (document.getElementById('pac-input'));
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+              google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.setContent(place.name);
+                  infowindow.open(map, this);
+              });
+          }
 
-        var searchBox = new google.maps.places.SearchBox((input));
+          function createCircle(place) {
+              var cicleOption = {
+                  strokeColor: 'white',
+                  strokeOpacity: 0.3,
+                  strokeWeight: 3,
+                  fillColor: '#ff5400',
+                  fillOpacity: 0.35,
+                  map: map,
+                  center: place.pos,
+                  radius: 1000
+              };
+              // Add the circle for this city to the map.
+              var cityCircle = new google.maps.Circle(cicleOption);
+          }
 
-        google.maps.event.addListener(searchBox, 'places_changed', function() {
+          function initialMapAndSearchBar(mapId, searchId) {
+
+              var pos = new google.maps.LatLng(25.0392179, 121.5398562);
+              var mapOptions = {
+                  zoom: 15,
+                  disableDefaultUI: true,
+                  center: pos,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+              };
+
+              map = new google.maps.Map(document.getElementById(mapId), mapOptions);
+              infowindow = new google.maps.InfoWindow();
+              google.maps.visualRefresh = true;
 
 
-            var places = searchBox.getPlaces();
+              var input = (document.getElementById(searchId));
+              // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-            // For each place, get the icon, place name, and location.
+              var searchBox = new google.maps.places.SearchBox((input));
 
-            var bounds = new google.maps.LatLngBounds();
+              google.maps.event.addListener(searchBox, 'places_changed', function () {
 
-            map.setCenter(places[0].geometry.location);
+                  var places = searchBox.getPlaces();
 
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: map,
-                title: places[0].name,
-                position: places[0].geometry.location,
-                animation: google.maps.Animation.DROP
-            });
+                  // For each place, get the icon, place name, and location.
 
-        });
+                  map.setCenter(places[0].geometry.location);
 
-    }
+                  createMarker({
+                      pos: places[0].geometry.location,
+                      name: places[0].name
+                  });
 
-    initialMap();
+              });
+
+              // Try HTML5 geolocation
+              if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(function (position) {
+                      var pos = new google.maps.LatLng(position.coords.latitude,
+                        position.coords.longitude);
+
+                      var infowindow = new google.maps.InfoWindow({
+                          content: 'I am here'
+                      });
+
+                      map.setCenter(pos);
+
+                      createMarker({
+                          pos: pos,
+                          name: 'I\'m here'
+                      });
+
+                      createCircle({
+                          pos: pos
+                      });
+
+                      utility.spinner.off();
+                  });
+
+
+              }
+
+              google.maps.event.addListener(map, 'idle', function () {
+                  // do something only the first time the map is loaded
+              });
+
+          }
+
+          google.maps.event.addDomListener(window, 'load', function () {
+              initialMapAndSearchBar('map-canvas', 'pac-input');
+          });
+
+
+
+      }]);
+
+
 })();
